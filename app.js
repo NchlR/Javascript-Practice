3df664ccece415f033ae01f56c2514f75d46e2a4
Nico <jndr051701@gmail.com>
2023-05-05 13:19:34 -0600

    Practice Javascript

---
+++ app.js
@@ -0,0 +1,31 @@
+const statusref = document.querySelector('.status')
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("VIP");
+    }, 2000);
+  });
+}
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+    resolve("show video")
+    }
+    else if (subscriptionStatus === "FREE") {
+    resolve("show trailer")
+    }
+    else {
+    reject("no video")
+    }
+  })
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusref.innerHTML = status;
+  console.log(await getVideo(status));
+}
+
+main();
