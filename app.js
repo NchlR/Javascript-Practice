09d9fe2e84469898a7a923c96ad6f7ed569e0ff7
Nico <jndr051701@gmail.com>
2023-05-07 15:47:50 -0600

    Complete

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const statusref = document.querySelector('.status')
+const statusref = document.querySelector(".status");
+const videoref = document.querySelector(".video");
 
 function getSubscriptionStatus() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("VIP");
+      resolve("none");
     }, 2000);
   });
 }
@@ -11,21 +12,25 @@ function getSubscriptionStatus() {
 function getVideo(subscriptionStatus) {
   return new Promise((resolve, reject) => {
     if (subscriptionStatus === "VIP") {
-    resolve("show video")
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
     }
-    else if (subscriptionStatus === "FREE") {
-    resolve("show trailer")
-    }
-    else {
-    reject("no video")
-    }
-  })
+  });
 }
 
 async function main() {
   const status = await getSubscriptionStatus();
   statusref.innerHTML = status;
-  console.log(await getVideo(status));
-}
 
+  try {
+    console.log(await getVideo(status));
+  } 
+  catch (e) {
+    console.log(e);
+    videoref.innerHTML = e;
+  }
+}
 main();
